a9eedc3a575436d2c8dced19708936c4b5fa288f
prrathnayake <pasanransika1@gmail.com>
2024-09-24 00:12:29 +1000

    updated

--- inventory_management/app/config.py
+++ inventory_management/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://temp_nl1b_user:UMvgg4jCa1ubHMMobWGlmfta94g0wNAf@dpg-crmv49tumphs73fa59p0-a.oregon-postgres.render.com/temp_nl1b')
